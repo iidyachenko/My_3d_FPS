9920f900b289b9b89ddc3ed50210024d61b7efb5
iidyachenko <rick_id@mail.ru>
2018-12-25 22:16:59 +0300

    Первая версия

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS
+{
+    public class InputController : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetButtonDown("SwitchFlashlight"))
+                Main.Instance.FlashlightController.SwitchFlashlight();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPS
+{
+    public class Main : MonoBehaviour
+    {
+        public static Main Instance { get; private set;}
+        public FlashlightController FlashlightController { get; private set; }
+        public InputController InputController { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance) DestroyImmediate(gameObject);
+            else Instance = this;
+        }
+
+        private void Start()
+        {
+            FlashlightController = gameObject.AddComponent<FlashlightController>();
+            InputController = gameObject.AddComponent<InputController>();
+        }
+
+    }
+}
+
